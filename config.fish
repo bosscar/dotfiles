5d97e8b66fde0f2d8cc473cc5b4ad630bfd582eb
bosscar <bosscar@gmail.com>
2020-07-14 16:12:28 -0700

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,3 +1,13 @@
+stty sane
+fish_vi_key_bindings
+set fish_greeting
+
+# bind
+bind \ce end-of-line accept-autosuggestion
+if bind -M insert > /dev/null 2>&1
+  bind -M insert \ce end-of-line accept-autosuggestion
+end
+
 abbr c clear
 abbr celar clear
 abbr cls clear
@@ -5,9 +15,26 @@ abbr x exit
 abbr t tree
 abbr l exa 
 abbr ll 'exa -lah'
+abbr v vim
+abbr h htop
+abbr go gotop
+abbr gl glances
 abbr rg ripgrep
 abbr frc 'vim ~/.config/fish/config.fish'
 abbr src 'source ~/.config/fish/config.fish'
+alias ..="cd .."
+alias .2="cd ../.."
+alias .3="cd ../../.."
+alias .4="cd ../../../.."
+alias .5="cd ../../../../.."
+alias .6="cd ../../../../../.."
+alias .7="cd ../../../../../../.."
+alias ls='~/bin/exa --group-directories-first'
+alias fzf='~/.fzf/bin/fzf'
+alias fzf-tmux='~/.fzf/bin/fzf-tmux'
+alias freeram='echo 3 | sudo tee /proc/sys/vm/drop_caches'
+
+export TERM=xterm-256color
 
 set PATH $PATH ~/.cargo/bin
 
@@ -16,3 +43,5 @@ function swp
   sudo apt full-upgrade -y
   sudo apt autoremove
 end
+
+if set -q TMUX; tmux setenv TMUXPWD_(tmux display -p "#D" | tr -d '%') $PWD; end
